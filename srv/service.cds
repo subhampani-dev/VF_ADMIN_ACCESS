89e6bda5c30c734b54f10aeca19f8e17cf187844
Subham Pani <subhampani1313@gmail.com>
2025-03-25 15:01:56 +0000

    Subaccount and Spaces code added - nontested

--- srv/service.cds
+++ srv/service.cds
@@ -2,4 +2,24 @@ using {ADMIN_MGMT as Tables} from '../db/schema';
 
 service ADM_service @(path: '/ADM_srv') @(requires: ['authenticated-user']) {
     entity TB_USER_PROFILE as projection on Tables.TB_USER_PROFILE;
+
+    @readonly
+    entity Subaccounts {
+        key subaccountId : String;
+        name            : String;
+        region          : String;
+    }
+
+    @readonly
+    entity Spaces {
+        key spaceId       : String;
+        name             : String;
+        subaccountId     : String;
+    }
+
+    @requires: 'authenticated-user'
+    function getSubaccounts() returns array of Subaccounts;
+
+    @requires: 'authenticated-user'
+    function getSpaces(subaccountId: String) returns array of Spaces;
 }
---
+++ srv/service.js
@@ -0,0 +1,56 @@
+const cds = require('@sap/cds');
+const axios = require('axios');
+const xsenv = require('@sap/xsenv');
+
+module.exports = cds.service.impl(async function () {
+    const { Subaccounts, Spaces } = this.entities;
+
+    // Load credentials dynamically from bound service (e.g., XSUAA)
+    const services = xsenv.getServices({ uaa: { tag: "ADMIN_ACCESS-auth" } });
+
+    async function getAccessToken() {
+        const response = await axios.post(services.uaa.url + "/oauth/token", null, {
+            auth: {
+                username: services.uaa.clientid,
+                password: services.uaa.clientsecret
+            },
+            params: { grant_type: "client_credentials" }
+        });
+        return response.data.access_token;
+    }
+
+    this.on('getSubaccounts', async () => {
+        const accessToken = await getAccessToken();
+
+        const response = await axios.get('https://accounts.cloud.sap/api/accounts/v1/subaccounts', {
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+
+        return response.data.value.map(acc => ({
+            subaccountId: acc.guid,
+            name: acc.displayName,
+            region: acc.region
+        }));
+    });
+
+    this.on('getSpaces', async (req) => {
+        const { subaccountId } = req.data;
+
+        if (!subaccountId) {
+            req.error(400, "subaccountId is required");
+            return;
+        }
+
+        const accessToken = await getAccessToken();
+
+        const response = await axios.get(`https://accounts.cloud.sap/api/accounts/v1/subaccounts/${subaccountId}/spaces`, {
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+
+        return response.data.value.map(space => ({
+            spaceId: space.guid,
+            name: space.name,
+            subaccountId: subaccountId
+        }));
+    });
+});
